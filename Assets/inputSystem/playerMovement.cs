c03a5b77b2d4c93126f2b34a9c64256c603be4a6
Termi <escarpaprieto@gmail.com>
2021-10-13 14:06:24 +0200

    Auto stash before revert of "terrain added"

--- Assets/inputSystem/playerMovement.cs
+++ Assets/inputSystem/playerMovement.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
+    CharacterController controller;
+    InputManager playerInput;
 
     float playerSpeed = 10f;
     bool isAlive = true;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        playerInput.Player.Movement.performed += _ => move();
     }
 
     // Update is called once per frame
@@ -22,9 +25,11 @@ public class playerMovement : MonoBehaviour
 
     void OnMovement()
     {
-        if (isAlive)
-        {
-            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
-        }
+
+    }
+
+    void move()
+    {
+        Debug.Log("moveeee!");
     }
 }
