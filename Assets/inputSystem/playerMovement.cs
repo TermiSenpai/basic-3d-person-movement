6323151458cc6d4275fce9d07bfdf638597d38c5
Termi <escarpaprieto@gmail.com>
2021-10-13 14:18:26 +0200

    change input manager

--- Assets/inputSystem/playerMovement.cs
+++ Assets/inputSystem/playerMovement.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
     CharacterController controller;
-    InputManager playerInput;
 
     float playerSpeed = 10f;
     bool isAlive = true;
@@ -14,22 +12,12 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        playerInput.Player.Movement.performed += _ => move();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void OnMovement()
-    {
-
-    }
-
-    void move()
-    {
-        Debug.Log("moveeee!");
     }
 }
