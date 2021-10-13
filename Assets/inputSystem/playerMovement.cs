5ea8a290865796da5def7e0aa93263bfe2ac9dc6
Termi <escarpaprieto@gmail.com>
2021-10-13 14:34:43 +0200

    player movement script

--- Assets/inputSystem/playerMovement.cs
+++ Assets/inputSystem/playerMovement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    CharacterController controller;
+    public CharacterController controller;
+    public Transform cam;
 
     float playerSpeed = 10f;
+    float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity = 0.1f;
     bool isAlive = true;
+
 
     // Start is called before the first frame update
     private void Awake()
     {
-       
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if ( direction.magnitude >= 1.0f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(movedir * playerSpeed * Time.deltaTime);
+        }
         
     }
 }
